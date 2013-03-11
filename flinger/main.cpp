3b70f102f1722435b882fc0b2caabc88e644068e
Mischa S <revmischa@cpan.org>
2013-03-10 23:19:25 -0700

    Initial Commit

---
+++ flinger/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  flinger
+//
+//  Created by Snooz on 3/10/13.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
